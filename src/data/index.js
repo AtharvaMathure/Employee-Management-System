1afbdbb5a7ad4159c5956cca63de11728880cc7e
AtharvaMathure <atharvamathure@gmail.com>
2022-11-25 10:53:23 +0530

    new updates

---
+++ src/data/index.js
@@ -0,0 +1,14 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Atharva',
+    lastName: 'Mathure',
+    email: 'athmat@gmail.com',
+    contact : '892818282',
+    salary: '10000',
+    date: '2019-04-11'
+  },
+
+];
+
+export { employeesData };
